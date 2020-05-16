ec30a42e86f94217873d5784b899217d5c9201be
Ananya2306 <amitarora_1@yahoo.com>
2020-05-16 20:47:01 +0530

    Slingshot.js

--- BaseClass.js
+++ BaseClass.js
@@ -2,7 +2,7 @@ class BaseClass{
     constructor(x, y, width, height, angle) {
         var options = {
             'restitution':0.8,
-            'friction':1.0,
+            'friction':0.4,
             'density':1.0
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
--- sketch.js
+++ sketch.js
@@ -24,13 +24,13 @@ function setup(){
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    log1 = new Log(810,260,250, PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
 
-    log3 =  new Log(810,180,300, PI/2);
+    log3 =  new Log(810,180,250, PI/2);
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
@@ -74,4 +74,10 @@ function mouseDragged(){
 
 function mouseReleased(){
     slingshot.fly();
+}
+
+function keyPressed(){
+    if(keyCode===32){
+        slingshot.attach(bird.body);
+    }
 }
